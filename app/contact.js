c99827e01221318424175b295f950b036a24d550
Ivan Kohan <kohan3750@gmail.com>
2026-01-16 16:27:00 +0100

    Update contact.js

--- app/contact.js
+++ app/contact.js
@@ -1,4 +1,4 @@
-export const LinkedIn_URL = "https://www.linkedin.com/in/ivan-kohan-3a2b39291";
+export const LinkedIn_URL = "https://www.linkedin.com/in/ivan-kohan/";
 export const Instagram_URL = "https://www.instagram.com/ivan.kohan.ua/";
 export const GitHub_URL = "https://github.com/Ivan3750";
 export const Email_URL = "mailto:kohan3750@gmail.com";
